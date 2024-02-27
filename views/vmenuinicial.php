1db449dd6737ecf5410a22569361e266b99ed006
Carlos <cmerinoribera@gmail.com>
2024-02-27 08:46:39 +0100

    Cambio de Menu Principal

--- views/vmenuinicial.php
+++ views/vmenuinicial.php
@@ -1,4 +1,4 @@
-<div id="contenedor">
-    <h1>PANEL DE ADMINISTRACION</h1>
+<div id="menuinicial">
+    <a href="index.php?controlador=ccontenedoresbasura&metodo=listadoContenedores" class="opcionesMenu"><img src="image/gestioncont.png" alt=""></a>
+    <a href="index.php?controlador=cbasura&metodo=eleccionGestion" class="opcionesMenu"><img src="image/gestionbasu.png" alt=""></a>
 </div>
-
